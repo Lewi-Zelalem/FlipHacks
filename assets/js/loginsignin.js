db9043a0363c855b333503540ebf3168004f2445
Arafat Mohammed <arafatmohammedbusi@gmail.com>
2024-10-11 19:12:39 +0300

    btns

--- assets/js/loginsignin.js
+++ assets/js/loginsignin.js
@@ -91,7 +91,7 @@ document
               .then(() => {
                 localStorage.setItem("username", username); // Store the username in local storage
                 window.location.href =
-                  "https://lewi-zelalem.github.io/Hackathon/"; // Redirect to index.html
+                  "https://lewi-zelalem.github.io/FlipHacks/userAcc.html"; // Redirect to index.html
               })
               .catch((error) => {
                 console.error("Error saving user data: ", error);
@@ -128,7 +128,7 @@ document
           if (userData.password === password) {
             // Replace with password comparison if hashed
             localStorage.setItem("username", username); // Store the username in local storage
-            window.location.href = "https://lewi-zelalem.github.io/Hackathon/"; // Redirect to index.html
+            window.location.href = "https://lewi-zelalem.github.io/FlipHacks/userAcc.html"; // Redirect to index.html
           } else {
             document.getElementById("error-message").innerText =
               "Invalid password!";
